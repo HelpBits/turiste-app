2dc26fbcf201071be2b5aff61d41fb65f44eb6e9
geraldvindas <gerald.vindas@ucr.ac.cr>
2021-01-23 16:15:14 -0600

    already done

--- src/screens/AddPostScreen.js
+++ src/screens/AddPostScreen.js
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Text, Image, View, ScrollView, StyleSheet, Alert } from 'react-native';
 import { Button, Input, Layout } from '@ui-kitten/components';
 import { launchImageLibrary } from 'react-native-image-picker';
 import storage from '@react-native-firebase/storage';
 import { withFirebaseHOC } from '../utils';
 
-class AddPost extends Component {
-  state = { image: null, description: '' };
+const AddPost = ({ firebase, challengePoint, setShowPostCreationModal }) => {
+  const [description, setDescription] = useState('');
+  const [image, setImage] = useState(null);
 
-  onChangeDescription = (description) => {
-    this.setState({ description });
-  };
-
-  setImageUrl = (reference) => {
+  const setImageUrl = (reference) => {
     reference
       .getDownloadURL()
       .then((res) => {
-        this.state.image.uri = res;
+        image.uri = res;
       })
       .catch((err) => console.log(err));
   };
 
-  uploadImageToStorage = (path) => {
+  const uploadImageToStorage = (path) => {
     let reference = storage().ref(
       `media/photos/posts/${Math.floor(Date.now())}`,
     );
@@ -29,44 +26,42 @@ class AddPost extends Component {
 
     task
       .then(() => {
-        this.setImageUrl(reference);
+        setImageUrl(reference);
       })
       .catch((e) => console.log('uploading image error => ', e));
   };
 
-  onSubmit = async () => {
-    if (this.state.image === null) {
+  const onSubmit = async () => {
+    if (image === null) {
       Alert.alert('Primero elige una imagen para el post.');
       return;
     }
-    if (this.state.description === '') {
+    if (description === '') {
       Alert.alert('Escribe algo sobre tu visita al lugar.');
       return;
     }
     try {
       const post = {
-        challengePointId: this.props.challengePoint.id,
-        photo: this.state.image.uri,
-        description: this.state.description,
+        challengePointId: challengePoint.id,
+        photo: image.uri,
+        description: description,
       };
 
-      await this.props.firebase.uploadPost(post);
+      await firebase.uploadPost(post);
 
       Alert.alert('Post subido correctamente');
 
-      this.setState({
-        image: null,
-        description: '',
-      });
+      setImage(null);
+      setDescription('');
 
-      this.props.setShowPostCreationModal(false);
+      setShowPostCreationModal(false);
     } catch (e) {
       Alert.alert('Ha ocurrido un error al subir el post');
       console.error('ERROR UPLOADING POST', e);
     }
   };
 
-  selectImage = () => {
+  const selectImage = () => {
     const options = {
       noData: true,
     };
@@ -79,78 +74,66 @@ class AddPost extends Component {
         console.log('User tapped custom button: ', response.customButton);
       } else {
         const source = { uri: response.uri };
-        this.uploadImageToStorage(response.uri);
-        this.setState({
-          image: source,
-        });
+        uploadImageToStorage(response.uri);
+        setImage(source);
       }
     });
   };
 
-  render() {
-    return (
-      <ScrollView style={{ flex: 1, marginTop: 60 }}>
-        <Text
+  return (
+    <ScrollView style={{ flex: 1, marginTop: 60 }}>
+      <Text
+        style={{
+          fontSize: 20,
+          alignSelf: 'center',
+          paddingBottom: 20,
+        }}>
+        Agregando un nuevo post al feed
+      </Text>
+      <View>
+        {image ? (
+          <Image source={image} style={{ width: '100%', height: 300 }} />
+        ) : (
+            <Button
+              appearance="outline"
+              status="primary"
+              onPress={selectImage}
+              style={{
+                alignItems: 'center',
+                padding: 10,
+                margin: 20,
+              }}>
+              + Agrega una imagen
+            </Button>
+          )}
+      </View>
+      <View style={{ marginTop: 20, alignItems: 'center' }}>
+        <Input
+          multiline={true}
+          placeholder="Escribe algo sobre tu visita..."
           style={{
-            fontSize: 20,
-            alignSelf: 'center',
-            paddingBottom: 20,
-          }}>
-          Agregando un nuevo post al feed
-        </Text>
-        <View>
-          {this.state.image ? (
-            <Image
-              source={this.state.image}
-              style={{ width: '100%', height: 300 }}
-            />
-          ) : (
-              <Button
-                appearance="outline"
-                status="primary"
-                onPress={this.selectImage}
-                style={{
-                  alignItems: 'center',
-                  padding: 10,
-                  margin: 20,
-                }}>
-                + Agrega una imagen
-              </Button>
-            )}
-        </View>
-        <View style={{ marginTop: 20, alignItems: 'center' }}>
-          <Input
-            multiline={true}
-            placeholder="Escribe algo sobre tu visita..."
-            style={{
-              marginLeft: 10,
-              marginRight: 10,
-              marginBottom: 30,
-            }}
-            value={this.state.description}
-            onChangeText={(description) =>
-              this.onChangeDescription(description)
-            }
-          />
-          <Layout style={styles.container} level="1">
-            <Button
-              style={styles.button}
-              status="basic"
-              onPress={() => this.props.setShowPostCreationModal(false)}>
-              Cancelar
-            </Button>
-            <Button
-              style={styles.button}
-              status="success"
-              onPress={this.onSubmit}>
-              Publicar
-            </Button>
-          </Layout>
-        </View>
-      </ScrollView>
-    );
-  }
-}
+            marginLeft: 10,
+            marginRight: 10,
+            marginBottom: 30,
+          }}
+          value={description}
+          onChangeText={(value) => setDescription(value)}
+        />
+        <Layout style={styles.container} level="1">
+          <Button
+            style={styles.button}
+            status="basic"
+            onPress={() => setShowPostCreationModal(false)}>
+            Cancelar
+          </Button>
+          <Button style={styles.button} status="success" onPress={onSubmit}>
+            Publicar
+          </Button>
+        </Layout>
+      </View>
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
